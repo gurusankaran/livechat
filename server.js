5b744b09f3bf13ab626a57d7283c97f44f15e032
Guru sankaran.k <146977761+gurusankaran@users.noreply.github.com>
2024-07-19 13:46:05 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.use(express.static('public'))
+// app.get('/',(req,res)=>{
+//     res.send('hi')
+// })
+
+const io = require('socket.io')(app.listen(port,()=>{
+    console.log(`Port listening on ${port}`);
+const io = require('socket.io')
+}))
+
+let socketConnected = new Set()
+
+io.on('connection',onConnected)
+
+function onConnected(socket){
+    console.log(socket.id);
+    socketConnected.add(socket.id)
+
+    io.emit('client-total',socketConnected.size)
+
+    socket.on('disconnect',()=>{
+        console.log('disconnected');
+        socketConnected.delete(socket.id)
+        io.emit('client-total',socketConnected.size)
+    })
+
+    socket.on('message',(data)=>{
+        console.log(data);
+        socket.broadcast.emit('chat-message',data)
+    })
+
+    socket.on('feedback',(data)=>{
+        socket.broadcast.emit('feedback',data)
+    })
+}
